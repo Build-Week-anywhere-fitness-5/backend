dcda32cf26e635290779ec8edbf38c1c3097dc31
Polly Yospan <porgroovy@gmail.com>
2021-05-27 21:28:58 -0400

    change date output return string to frontend

--- api/data/routers/classes/classes-model.js
+++ api/data/routers/classes/classes-model.js
@@ -1,7 +1,40 @@
 const db = require("../../db-config");
 
 async function getClasses() {
-  return db("classes");
+  const getDate = await db("classes");
+
+  // .select(
+  //   "class_name",
+  //   "type",
+  //   "date",
+  //   "start_time",
+  //   "duration_mins",
+  //   "intensity",
+  //   "location",
+  //   "current_registered",
+  //   "max_class_size"
+  // );
+
+  // const date =
+
+  return getDate.map((e) => {
+    return {
+      class_name: e.class_name,
+      type: e.type,
+      date: String(e.date).split(
+        " 00:00:00 GMT-0400 (Eastern Daylight Time)"
+      )[0],
+      start_time: e.start_time,
+      duration_mins: e.duration_mins,
+      intensity: e.intensity,
+      location: e.location,
+      current_registered: e.current_registered,
+      max_class_size: e.max_class_size,
+    };
+  });
+  //
+  //return date;
+  //console.log(getDate);
 }
 
 async function getClassById(class_id) {
